ce8c77861da6286189378cd2693f0db7a0becb07
Jasper-field <jaspersongfield@gmail.com>
2024-09-16 21:00:51 +0000

    working on it

--- BankAccount.java
+++ BankAccount.java
@@ -1,5 +1,53 @@
 public class BankAccount
-  {
+{
+private String name;
+private int accountNum;
+private double balance;
+private String password;
+private boolean loggedIn;
 
-    
+public BankAccount(String name, int acc, double balance, String password){
+  this.name=name;
+  this.accountNum=acc;
+  this.balance=balance;
+  this.password=password;
+  this.loggedIn=false;
+}
+public boolean logIn(String username, String password)
+{
+  if (username.equals(this.name) && password.equals(this.password))
+  {
+    loggedIn=true;
+    return true;
   }
+  else
+  {
+    return false;
+  }
+}
+public double getBalance()
+{
+  if (loggedIn==true)
+  {
+    return balance;
+  }
+  else
+  {
+    throw new IllegalStateException("Not Logged In");
+  }
+}
+  public double deposit(double amount)
+  {
+    if (loggedIn==true)
+    {
+      this.balance+=amount;
+      return this.balance;
+    }
+    else 
+    {
+      throw new IllegalStateException("Not Logged In");
+    }
+  }
+}
+
+
---
+++ test.java
@@ -0,0 +1,12 @@
+public class test
+{
+public static void main (String[] args)
+{
+    BankAccount checking = new BankAccount("jsong",123,100.0,"mark");
+    checking.logIn("jsong", "mark");
+    System.out.println(checking);
+    checking.deposit(100);
+    System.out.println();
+}
+
+}
